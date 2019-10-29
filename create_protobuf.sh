43cf481bb11fab1fff91205d009468238989e96a
Thorben Jensen <jensen.thorben@gmail.com>
2019-10-29 21:45:22 +0100

    setting up creation of tfrecord files

---
+++ create_protobuf.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+# From tensorflow/models/research/
+cd models/research
+PYTHONPATH=`pwd`
+echo $PYTHONPATH
+
+# wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+# wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+# tar -xvf annotations.tar.gz
+# tar -xvf images.tar.gz
+python ../../create_pet_tf_record.py \
+    --label_map_path=object_detection/data/pet_label_map.pbtxt \
+    --data_dir=`pwd` \
+    --output_dir=`pwd`
