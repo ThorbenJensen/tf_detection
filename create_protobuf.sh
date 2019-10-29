7e7e59e9a209ff3109501e1953f69f1d0e3c07a7
Thorben Jensen <jensen.thorben@gmail.com>
2019-10-29 21:47:31 +0100

    adding download of pet data to script

--- create_protobuf.sh
+++ create_protobuf.sh
@@ -5,10 +5,10 @@ cd models/research
 PYTHONPATH=`pwd`
 echo $PYTHONPATH
 
-# wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
-# wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
-# tar -xvf annotations.tar.gz
-# tar -xvf images.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+tar -xvf annotations.tar.gz
+tar -xvf images.tar.gz
 python ../../create_pet_tf_record.py \
     --label_map_path=object_detection/data/pet_label_map.pbtxt \
     --data_dir=`pwd` \
